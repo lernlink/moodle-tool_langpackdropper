4c639a55664af715b5db1408cf7eea606da7cfed
Alexander Bias <alexander.bias@lernlink.de>
2022-05-30 20:27:19 +0200

    Remove a surplus space sign

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
- defined('MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_langpackdropper';
 $plugin->version = 2022022701;
