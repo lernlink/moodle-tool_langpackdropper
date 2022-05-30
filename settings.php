5634ce945d632810aeaf6fedd9968e307e5cba34
Alexander Bias <alexander.bias@lernlink.de>
2022-05-30 21:13:18 +0200

    Improvement: Add note that language packs will not be removed again

--- settings.php
+++ settings.php
@@ -40,9 +40,11 @@ if ($hassiteconfig) {
         $settings->add($setting);
 
         // Create language pack urls widget.
+        $manageurl = new moodle_url('/admin/tool/langimport/index.php');
         $setting = new admin_setting_configtextarea('tool_langpackdropper/langpackurls',
                 get_string('setting_langpackurls', 'tool_langpackdropper', null, true),
-                get_string('setting_langpackurls_desc', 'tool_langpackdropper', null, true), '', PARAM_RAW);
+                get_string('setting_langpackurls_desc', 'tool_langpackdropper', array('managepage' => $manageurl->out()), true),
+                        '', PARAM_RAW);
         $setting->set_updatedcallback('tool_langpackdropper_updatecallback');
         $settings->add($setting);
 
